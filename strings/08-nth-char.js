2ca9ae65dc85a1af624cb6286439ae7ffa7bef59
chanchal <rajputchanchal17@gmail.com>
2023-01-17 17:18:42 +0530

    create - get nth char

---
+++ strings/08-nth-char.js
@@ -0,0 +1,14 @@
+// Write a function that takes a string (a) and a number (n) as argument. Return the nth character of 'a'.
+
+const nthChar = (str,n) =>{
+    for(let i=0; i<str.length;i++){
+        if(i === n){
+           return str[i]
+        }
+     }
+
+    // 2nd way
+    // return str.charAt(n)
+ 
+}
+console.log(nthChar('abc',2))
