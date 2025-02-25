6c8f6c1025248723f253dafd6aae841869da71af
chanchal16 <rajputchanchal17@gmail.com>
2025-02-25 17:01:14 +0530

    update: add two pointers approach

--- strings/04-reverse-char-each-word.js
+++ strings/04-reverse-char-each-word.js
@@ -3,13 +3,35 @@
 Example:
 Input: reverseCharactersOfWord('we are neoGrammers') –––> Output: ew era sremmarGoen */
 
-const reverseCharOfWord = (sentence)=>{
-    let words = sentence.split(' ');
-    let result = words.map(word=>{
-        let chars = word.split('').reverse().join('')
-        // console.log('chars',chars);
-        return chars
-    }).join(' ')
-    return result;
-}
-console.log('reversed - ',reverseCharOfWord('we are neoGrammers'))
+const reverseCharOfWord = (sentence) => {
+  let words = sentence.split(" ");
+  let result = words
+    .map((word) => {
+      let chars = word.split("").reverse().join("");
+      // console.log('chars',chars);
+      return chars;
+    })
+    .join(" ");
+  return result;
+};
+console.log("reversed - ", reverseCharOfWord("we are neoGrammers"));
+
+/* Reverse a string - Write a function to reverse a string without using extra space.
+ input - good moring!
+ output - !gnirom doog
+*/
+
+const reverseString = (str) => {
+  let words = str.split("");
+  // using two pointer
+  let start = 0;
+  let end = words.length - 1;
+
+  while (start < end) {
+    [words[start], words[end]] = [words[end], words[start]];
+    start++;
+    end--;
+  }
+  return words.join("");
+};
+console.log("reverse string:", reverseString("good morning"));
